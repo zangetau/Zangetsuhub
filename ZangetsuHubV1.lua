693d0b207f151a80426980c1f6fbc2e35d23bf8f
zangetau <chasonclark2012@gmail.com>
2025-04-06 18:03:23 -0600

    Update ZangetsuHubV1.lua

--- ZangetsuHubV1.lua
+++ ZangetsuHubV1.lua
@@ -1,20 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/zangetau/Zangetsuhub/main/ui.lua"))()
-
-
-
-
--- Zangetsu Hub V1 Loader
-repeat wait() until game:IsLoaded()
-local player = game.Players.LocalPlayer
-local StarterGui = game:GetService("StarterGui")
-
-StarterGui:SetCore("SendNotification", {
-	Title = "Zangetsu Hub V1";
-	Text = "Loading Zangetsu Hub...";
-	Duration = 3;
-})
-
-wait(2)
-
--- Load actual hub
+-- ZangetsuHubV1.lua
 loadstring(game:HttpGet("https://raw.githubusercontent.com/zangetau/Zangetsuhub/main/ui.lua"))()
